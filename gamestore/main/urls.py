fa36f8baf384ff33060f171000dfac63ee77fe19
Víctor Andrés Rojas <victandres98@gmail.com>
2023-03-08 17:28:35 -0500

    Creating Shopping Cart view - Updated urls.py file - Created cart.html file

--- gamestore/main/urls.py
+++ gamestore/main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path(r'accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path(r'accounts/signup/', views.signup, name='signup'),
     path(r'games-list/highlighted/', views.show_highlighted_games),
-    path(r'games-list/all/', views.show_all_games)
+    path(r'games-list/all/', views.show_all_games),
+    path(r'cart/', views.ShoppingCartUpdateView.as_view(), name='user-cart')
 ]
